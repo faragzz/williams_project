81c58f198bb5e7428cbbea77c348b6f86a47a207
faragzz <a.khaled46462@gmail.com>
2024-11-19 11:34:23 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="container">
+
+    <div class="partOne">
+      <div class="nav">
+        <Nav :active="active" @update:active="updateActive"/>
+        <div style="display: flex; gap: 8px; justify-content: center;">
+          <img :src="arrowLeft" width="24px" height="24px" v-show="active !== 0" @click="prevItem" />
+          <img :src="arrowRight" width="24px" height="24px" v-show="active !== 5" @click="nextItem"/>
+        </div>
+      </div>
+    </div>
+    <div class="partTwo">
+
+    </div>
+  </div>
+</template>
+
+<script scoped>
+import Nav from './components/Nav.vue';
+import arrowRight from '../src/assets/icons/right.svg'
+import arrowLeft from '../src/assets/icons/left.svg'
+export default {
+  components: {
+    Nav,
+  },
+  data() {
+    return {
+      active: 0,
+      arrowRight,
+      arrowLeft
+    };
+  },
+  methods: {
+    updateActive(newActive) {
+      this.active = newActive;
+    },
+    prevItem() {
+      if (this.active > 0) {
+        this.active--;
+      } else {
+        this.active = 5; // Wrap to the last item
+      }
+    },
+    nextItem() {
+      if (this.active < 5) {
+        this.active++;
+      } else {
+        this.active = 0; // Wrap to the first item
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  position: relative;
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+}
+.partOne{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: lemonchiffon;
+  width: 50vw;
+  height: 100vh;
+}
+.partTwo{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: cyan;
+  width: 50vw;
+  height: 100vh;
+}
+.nav {
+  position: absolute;
+  bottom: 50px;
+  display: flex;
+  flex-direction: column;
+  gap:32px;
+}
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="con">
+    <span
+        v-for="(label, index) in ['Service', 'Asset Management', 'Acquisitions & Financing', 'Property Management', 'Leasing', 'Legal']"
+        :key="index"
+        :class="{ active: index === activeIndex }"
+        @click="setActive(index)"
+    >
+      {{ label }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    active: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      activeIndex: this.active,
+    };
+  },
+  watch: {
+    active(newActive) {
+      this.activeIndex = newActive;
+    },
+  },
+  methods: {
+    setActive(index) {
+      this.activeIndex = index;
+      this.$emit("update:active", index);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.con {
+  display: flex;
+  gap: 16px;
+}
+
+span {
+  font-size: 14px;
+  color: gray;
+  cursor: pointer;
+  text-decoration: none;
+  padding: 4px 0;
+  transition: color 0.3s ease;
+}
+
+span:hover {
+  color: black;
+}
+
+span.active {
+  color: black;
+}
+
+span::after {
+  content: '';
+  display: block;
+  width: 0;
+  height: 2px;
+  background-color: #007bff;
+  margin-top: 4px;
+  transition: width 0.3s ease;
+}
+
+span:hover::after {
+  width: 100%;
+}
+
+span.active::after {
+  width: 100%;
+}
+
+</style>
